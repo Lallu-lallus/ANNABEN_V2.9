a8b4747fcf72a7489010cda38e7e5c1ab35739dd
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-03 10:43:56 +0530

    Update __main__.py

--- Annaben_robot/__main__.py
+++ Annaben_robot/__main__.py
@@ -4,7 +4,7 @@
 # All rights reserved by PR0FESS0R-99
 # License -> https://github.com/PR0FESS0R-99/DonLee_Robot/blob/main/LICENSE
 
-from .Annaben_robot import Annaben_robot
+from .Donlee_robot import Annaben_robot
 
 app = Annaben_robot()
 app.run()
